f34de791c1c66daf98921ad9dace99b484b9ae13
frank10gm <frank10gm@gmail.com>
2016-09-13 15:42:55 +0200

    ok

--- ParkX3/ParkX3/About.xaml.cs
+++ ParkX3/ParkX3/About.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,12 +8,12 @@ using Xamarin.Forms;
 
 namespace ParkX3
 {
-    public partial class About : ContentPage
-    {
-        public About(string data)
-        {
-			
-            InitializeComponent();
-        }
-    }
+	public partial class About : ContentPage
+	{
+		public About(string data)
+		{
+			InitializeComponent();
+			gino.Text = data;
+		}
+	}
 }
